fa569cda58a04013a5f0d478897c96be1c3a72b5
Enes Musab Doğan <enesmusab.dogan2018@pru.edu.tr>
2021-05-11 22:50:10 +0300

    update4

---
+++ CinemaHub/Models/Seans.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CinemaHub.Models
+{
+    public class Seans
+
+    {
+        [Key]
+        public int ID { get; set; }
+        public int SAAT { get; set; }
+    }
+}
