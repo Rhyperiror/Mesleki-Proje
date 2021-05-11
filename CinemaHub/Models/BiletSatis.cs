8526bf33fee4e18384372e963014fc3d48017481
Enes Musab Doğan <enesmusab.dogan2018@pru.edu.tr>
2021-05-11 22:57:38 +0300

    UPDATE6

---
+++ CinemaHub/Models/BiletSatis.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CinemaHub.Models
+{
+    public class BiletSatis
+    {
+        [Key]
+        public int BILETID { get; set; }
+        [Key]
+        public int SATISID { get; set; }
+    }
+}
