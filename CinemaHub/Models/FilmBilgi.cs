0ca69a4cd3f09fc7934c1785397a857a1071c3d8
Enes Musab Doğan <enesmusab.dogan2018@pru.edu.tr>
2021-05-11 22:46:10 +0300

    update2

---
+++ CinemaHub/Models/FilmBilgi.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CinemaHub.Models
+{
+    public class FilmBilgi
+    {
+        [Key]
+        public int ID { get; set; }
+        public string FILMADI { get; set; }
+        public string FILMTURU { get; set; }
+        public string YONETMEN { get; set; }
+        public string OYNUCULAR { get; set; }
+        public int VIZYONTARIH { get; set; }
+        public int SURE { get; set; }
+        public int YAPIMTARIH { get; set; }
+        public string ULKE { get; set; }
+    }
+}
