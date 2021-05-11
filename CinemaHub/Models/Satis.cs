30e8469c3218b22df16bddb27261881c1941755c
Enes Musab Doğan <enesmusab.dogan2018@pru.edu.tr>
2021-05-11 22:54:49 +0300

    update6

---
+++ CinemaHub/Models/Satis.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CinemaHub.Models
+{
+    public class Satis
+    {
+        [Key]
+        public int ID { get; set; }
+        public int TARIH { get; set; }
+        public int MUSTERIID { get; set; }
+        public int FIYAT { get; set; }
+    }
+}
