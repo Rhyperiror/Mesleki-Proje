d6090ef4ff7fcc777054c242964cfc51473815e6
Enes Musab Doğan <enesmusab.dogan2018@pru.edu.tr>
2021-05-11 22:48:28 +0300

    update3

---
+++ CinemaHub/Models/Salon.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CinemaHub.Models
+{
+    public class Salon
+    {
+        [Key]
+        public int ID { get; set; }
+        public string SALONADI { get; set; }
+        public int KAPASITE { get; set; }
+        public int KOLTUKNUMARA { get; set; }
+    }
+}
